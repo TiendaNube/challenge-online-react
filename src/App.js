15c1bc89345c11b8d9dc697c684ab3fe00316ac3
Nicolás Croce <ingcrocenicolas@outlook.com>
2020-07-22 20:23:17 -0300

    Feature/layout (#4)
    * feat(scss): The styles was updated.
    
    * feat(js): Added new layout and ProductForm.
    
    * feat(scss): The layout was defined.
    
    * feat(chore): Added google font.
    
    * feat(svg): Added image.
    
    * feat(js, scss): The layout was updated.
    
    Co-authored-by: Nicolás Croce <nicolas.croce@tiendanube.com>

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import './styles.scss';
-import { Switch, Route, Link } from "react-router-dom";
-//import ProductForm from './components/ProductForm';
+import { Switch, Route, NavLink } from "react-router-dom";
+import ProductForm from './components/ProductForm';
 
-import "./App.css";
+import {ReactComponent as TnLogo} from './img/tn.svg';
 
 const A = () => <div>Componente 1</div>;
-const B = () => <div>Componente 2</div>;
 
 function App() {
   return (
-    <div className="App">
+    <div className="app layout">
       <aside>
+        <header> <TnLogo className="svg"/> </header>
         <nav>
           <ul>
             <li>
-              <Link to="/">Product List</Link>
+              <NavLink to="/" activeClassName="active" exact>Product List</NavLink>
             </li>
             <li>
-              <Link to="/products">Products</Link>
+              <NavLink to="/products" activeClassName="active">Products</NavLink>
             </li>
           </ul>
         </nav>
       </aside>
       <main>
+        <header className="page-title">
+          <h1>Products</h1>
+        </header>
         <Switch>
           <Route exact path="/" component={A} />
-          <Route path="/products" component={B} />
+          <Route exact path="/products" component={ProductForm} />
         </Switch>
       </main>
     </div>
