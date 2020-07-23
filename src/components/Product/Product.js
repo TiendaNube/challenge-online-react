5c136038c56acc21a6cc2a4263ce8d6510df85c9
Nicolás Croce <nicolas.croce@tiendanube.com>
2020-07-23 19:55:40 -0300

    feat(js, scss): Added Product component.

---
+++ src/components/Product/Product.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./Product.scss";
+
+const Product = ({ name, count, price, promotionalPrice }) => {
+  return (
+    <>
+      <div className="product">
+        <span>{name}</span>
+        <span>{count}</span>
+        <span>{price}</span>
+        <span>{promotionalPrice}</span>
+      </div>
+    </>
+  );
+};
+
+export default Product;
