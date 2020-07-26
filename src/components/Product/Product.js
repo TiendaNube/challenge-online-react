b389b570f6f7e75cd5ee5e80708b361f6fd9b208
Nicolás Croce <ingcrocenicolas@outlook.com>
2020-07-26 10:08:32 -0300

    feat(*): Added test and one case where is braking. (#6)
    Co-authored-by: Nicolás Croce <nicolas.croce@tiendanube.com>

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,16 +2,13 @@ import React from "react";
 
 import "./Product.scss";
 
-const Product = ({ name, count, price, promotionalPrice }) => {
+const Product = ({ name, count, price, callback }) => {
   return (
-    <>
-      <div className="product">
-        <span>{name}</span>
-        <span>{count}</span>
-        <span>{price}</span>
-        <span>{promotionalPrice}</span>
-      </div>
-    </>
+    <div className="product">
+      <span>{name}</span>
+      <span>{count}</span>
+      <span>{price}</span>
+    </div>
   );
 };
 
---
+++ src/components/Product/product.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+
+import Product from "./Product";
+
+const data = { name: "T-shirt", count: 3, price: 20, promotionalPrice: 15 };
+
+describe("Render Product", () => {
+  function renderProduct (callback) {
+    render(
+      <Product
+        name={data.name}
+        count={data.count}
+        price={data.price}
+        promotionalPrice={data.promotionalPrice}
+        callback={callback}
+      />
+    );
+  }
+  it("Render Name", () => {
+    renderProduct();
+    expect(screen.getByText(data.name)).toBeInTheDocument();
+  });
+  it("Render Count", () => {
+    renderProduct();
+    expect(screen.getByText(data.count.toString())).toBeInTheDocument();
+  });
+  it("Render Price", () => {
+    renderProduct();
+    expect(screen.getByText(data.price.toString())).toBeInTheDocument();
+  });
+  it("Render Promotional Price", () => {
+    renderProduct();
+    expect(screen.getByText(data.promotionalPrice.toString())).toBeInTheDocument();
+  });
+});
