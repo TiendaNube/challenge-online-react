a91f791efecd9bc7cfc01823f518351c3bafac11
Nicolás Croce <nicolas.croce@tiendanube.com>
2020-07-27 00:36:16 -0300

    feat(js): Added some errors.

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -14,12 +14,11 @@ class ProductForm extends Component {
   }
 
   handleInputChange = (event) => {
-    return this.setState({ [event.target.name] : event.target.value })
+    return this.setState(event.target.value)
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state);
   }
 
   render() {
@@ -35,7 +34,7 @@ class ProductForm extends Component {
         <input name="price" type="text" value={ this.state.price } onChange={this.handleInputChange}/>
         <label className="label" htmlFor="">Promotional Price</label>
         <input name="promotionalPrice" type="text" value={ this.state.promotionalPrice } onChange={this.handleInputChange}/>
-        <input className="button" type="submit" value="Aceptar" onClick={this.handleSubmit}/>
+        <input className="button" type="submit" value="Aceptar" />
       </form>
       </div>
     );
